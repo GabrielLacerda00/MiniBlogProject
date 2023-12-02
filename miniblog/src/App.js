086e335925a6ce66634ce795f96ccf6c4707471d
Gabriel Lacerda <gabriellacerda@MacBook-Air-de-Gabriel.local>
2023-12-02 12:18:55 -0300

    Config routers and firebase

---
+++ miniblog/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+import {Routes, BrowserRouter, Route, Navigate} from "react-router-dom";
+
+//Import routes pages
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+              <Route path='/' element={<Home/>}/>
+              <Route path='/about' element={<About/>}/>
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ miniblog/src/firebase/config.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+import {getFirestore} from "firebase/firebase";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAyVk4OEpu41lzExOWX1IUOGW921Gu2yqk",
+  authDomain: "miniblog-3c1b5.firebaseapp.com",
+  projectId: "miniblog-3c1b5",
+  storageBucket: "miniblog-3c1b5.appspot.com",
+  messagingSenderId: "613761794725",
+  appId: "1:613761794725:web:3b9ccc14a0d8f42e5c834c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+//Initialize DataBase
+const db = getFirestore(app)
+
+export {db};
